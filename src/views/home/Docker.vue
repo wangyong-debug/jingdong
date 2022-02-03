adced7b1e104ab184f6ea911146860de2e95ff0c
wangyong-debug <869270569@qq.com>
2022-02-03 20:44:11 +0800

    首页组件的合理拆分

---
+++ src/views/home/Docker.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6b8;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe60e;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe78b;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: ' Docker '
+}
+</script>
+
+<style lang="scss">
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 0.01px solid $content-bgColor;
+  color: $content-fontcolor;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &__title {
+    font-size: 0.2rem;
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
+}
+</style>
